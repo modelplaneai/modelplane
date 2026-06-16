f24f1fa86fa81f871c02fc9864be4b63138ff9d2
Rae Sharp <resharp20@gmail.com>
2026-06-16 10:10:10 -0400

    Update manifests path to root. Update shortcode path for consistency
    Signed-off-by: Rae Sharp <resharp20@gmail.com>

--- nix/docs.nix
+++ nix/docs.nix
@@ -87,6 +87,7 @@ in
       ''
         cp -r ${self}/docs src
         cp -r ${self}/apis apis
+        cp -r ${self}/examples examples
         chmod -R u+w src
         cd src
         ln -s ${nodeModules}/node_modules node_modules
